ca6b3c2edef706720f0e4d71ac724d14fc73dd10
NguyenMinhTruong-dev <nguyenminhtruongdev@gmail.com>
2024-07-02 00:41:39 +0700

    commit implement increase/decrease function with bloc pattern

---
+++ lib/app_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterblocapplication/app_events.dart';
+
+import 'app_states.dart';
+
+class AppBloc extends Bloc<AppEvents, AppStates>{
+  AppBloc():super(InitStates()){
+
+    on<Increment>((event, emit){
+
+      // _incrementCounter(emit);
+      emit(AppStates(counter: state.counter+1));
+      print('FlutterApp: _incrementCounter "${state.counter}"');
+    });
+
+    on<Decrement>((event, emit){
+      // _decrementCounter(emit);
+      emit(AppStates(counter: state.counter-1));
+      print('FlutterApp: _decrementCounter "${state.counter}"');
+    });
+  }
+
+  void _incrementCounter(Emitter<AppStates> emit) {
+    emit(AppStates(counter: state.counter+1));
+  }
+
+  void _decrementCounter(Emitter<AppStates> emit) {
+    emit(AppStates(counter: state.counter-1));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterblocapplication/app_bloc.dart';
+import 'package:flutterblocapplication/app_events.dart';
+import 'package:flutterblocapplication/app_states.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,13 +15,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context)=>AppBloc(),
+    child: const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Flutter Demo Home Page',),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -31,19 +35,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,28 +45,33 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: BlocBuilder<AppBloc, AppStates>(
+          builder: (context, state) {
+            print('FlutterApp: builder');
+            return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    "${BlocProvider.of<AppBloc>(context).state.counter}",
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+              );
+          },
+        )
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [FloatingActionButton(
-          onPressed: _decrementCounter,
+          onPressed: () => BlocProvider.of<AppBloc>(context).add(Decrement()),
           tooltip: 'Decrement',
           child: const Icon(Icons.remove),
         ),
           FloatingActionButton(
-            onPressed: _incrementCounter,
+            onPressed: () => BlocProvider.of<AppBloc>(context).add(Increment()),
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           )],
